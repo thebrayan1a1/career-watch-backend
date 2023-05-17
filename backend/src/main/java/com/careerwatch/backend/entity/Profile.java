6a44a26c71c730e8fb7a8591d2954a048075cf7e
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-05-17 20:34:44 -0300

    SP1-CW30: Creada entidad Profile

---
+++ backend/src/main/java/com/careerwatch/backend/entity/Profile.java
@@ -0,0 +1,45 @@
+package com.careerwatch.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "RESUME_ID", referencedColumnName = "ID")
+    private Resume resume;
+
+    @Column(name = "FULL_NAME")
+    private String fullName;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "Location")
+    private String location;
+
+    @Column(name = "IMG_RESUME")
+    private String imgResume;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "PROFILE_ID", referencedColumnName = "ID")
+    private List<Social> socials;
+}
