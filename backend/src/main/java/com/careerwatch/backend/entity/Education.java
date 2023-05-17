ff6f5bb72a94df53f1c06fc2c7bd02832425fd6d
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-05-17 20:33:38 -0300

    SP1-CW30: Creada entidad Education

---
+++ backend/src/main/java/com/careerwatch/backend/entity/Education.java
@@ -0,0 +1,40 @@
+package com.careerwatch.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Education {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @NotNull
+    @Column(name = "RESUME_ID")
+    private Long resumeId;
+
+    @NotNull
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "INSTITUTION")
+    private String institution;
+
+    @Column(name = "DATE_START")
+    private String dateStart;
+
+    @Column(name = "DATE_END")
+    private String dateEnd;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+}
