6d20fefd47fb7a7bfe8c218172534caac82e1673
Jiao <60641358+Jiao-lin@users.noreply.github.com>
2023-05-17 18:12:55 -0300

    tailwind en frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-neutral-100 w-full h-14'></header>
+  )
+}
+
+export default Header
---
+++ frontend/src/views/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function Landing() {
+  return (
+    <>
+    <Header/>
+     <div className='bg-white text-black w-full h-24'>landing</div>
+    </>
+  )
+}
+
