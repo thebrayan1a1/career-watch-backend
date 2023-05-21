ae7fee80e9387e883692ce1e41d90e6c189a7e5a
brayanrodallega <thebrayan1a1@outlook.com>
2023-05-21 10:12:19 -0500

    SP1-CW34: arreglado nombre del controlador de education

--- backend/src/main/java/com/careerwatch/backend/controller/educationController.java
+++ backend/src/main/java/com/careerwatch/backend/controller/educationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/educations")
-public class educationController {
+public class EducationController {
     
     private final educationService educationService;
     
